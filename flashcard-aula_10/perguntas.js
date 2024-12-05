5a6d682a2a99f4bb8056c672acf4e2d23710010f
Maria01924 <fmariafernanda733@gmail.com>
2024-12-05 07:39:14 -0300

    Add files via upload

---
+++ flashcard-aula_10/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é windows?',
+    'O windows e um sistema operacional'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da italia',
+    'A capital da italia e a roma'
+)
+
+criaCartao(
+    'Programação',
+    'qual e a diferenca entre java e python?',
+    'java e python sao linguagens de programacao distintas'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz boa tarde em ingles?',
+    'boa tarde em ingles é good afternoon'
+)
